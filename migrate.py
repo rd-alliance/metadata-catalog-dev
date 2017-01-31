39a92e5390c3d0f93a26f3a6dc562692f960420b
Alex Ball <alex-ball@users.noreply.github.com>
2017-01-31 14:59:49 +0000

    Improve stability of ID assignment, write messages to log

--- migrate.py
+++ migrate.py
@@ -2,7 +2,7 @@
 
 ### Dependencies
 
-import argparse, os, sys, yaml, re
+import argparse, os, sys, yaml, re, datetime
 
 ### Initializing
 
@@ -10,22 +10,80 @@ import argparse, os, sys, yaml, re
 
 default_source = os.path.realpath(os.path.join(os.path.dirname(sys.argv[0]), '..', 'metadata-directory'))
 default_dest = os.path.realpath(os.path.join(os.path.dirname(sys.argv[0]), 'db'))
+log_file = os.path.realpath(os.path.join(os.path.dirname(sys.argv[0]), 'migration-log.txt'))
 
 ## Command-line arguments
 
 parser = argparse.ArgumentParser(description='''
-Converts RDA metadata standards directory data into the new RDA metadata standards catalog model.''')
+Converts RDA metadata standards directory data into the new RDA metadata
+standards catalog data model, ready for importing into a NoSQL database.''')
 parser.add_argument('-f', '--from'\
-    ,help='Path of MSD data files'\
+    ,help='Location of MSD data files'\
     ,action='store'\
     ,default=default_source\
     ,dest='source')
 parser.add_argument('-t', '--to'\
-    ,help='Path of MSC data files'\
+    ,help='Location of MSC data files'\
     ,action='store'\
     ,default=default_dest\
     ,dest='dest')
 args = parser.parse_args()
+
+## Utility functions
+
+def getMSCID(path):
+    if os.path.isfile(path):
+        with open(path, 'r') as r:
+            dest_records = yaml.safe_load_all(r)
+            dest_record = next(dest_records)
+            for identifier in dest_record['identifiers']:
+                if identifier['scheme'] == 'RDA-MSCWG':
+                    return identifier['id']
+    else:
+        return None
+
+def loadRecord(path):
+    if os.path.isfile(path):
+        with open(path, 'r') as r:
+            dest_records = yaml.safe_load_all(r)
+            dest_record = next(dest_records)
+            return dest_record
+    else:
+        return None
+
+# TODO: define vocabulary mapping
+def translateKeyword(kw):
+    output = None
+    return output
+
+def createSlug(string):
+    output = string.strip().lower().replace(' ', '-')
+    output = re.sub(r'-+', '-', output)
+    output = re.sub(r'[^-A-Za-z0-9_]+', '', output)
+    return output
+
+## Utility variables
+
+# Lookup for slug -> new ID
+m_index = dict()
+g_index = dict()
+t_index = dict()
+
+# Collecting information for post-processing
+mappings = list()
+sponsors = list()
+contacts = list()
+
+# Incremental ID integers
+m = 0
+g = 0
+t = 0
+c = 0
+e = 0
+
+# Log file contents
+log = ''
+isNewLog = False
 
 ### Processing
 
@@ -55,51 +113,41 @@ tools = getRecords('tools')
 users = getRecords('use_cases')
 
 print('Checking {} for data structure...'.format(args.dest))
-for folder in [ 'metadata-schemes', 'organizations', 'tools', 'mappings', 'endorsements' ]:
-    if not os.path.isdir(os.path.join(args.dest, folder)):
+for series, folder in { 'm': 'metadata-schemes', 'g': 'organizations', 't': 'tools', 'c': 'mappings', 'e': 'endorsements' }.items():
+    if os.path.isdir(os.path.join(args.dest, folder)):
+        serial_no = 0
+        for entry in os.listdir(os.path.join(args.dest, folder)):
+            if (os.path.splitext(entry)[1] != '.yml'):
+                continue
+            with open(os.path.join(args.dest, folder, entry), 'r') as r:
+                dest_records = yaml.safe_load_all(r)
+                dest_record = next(dest_records)
+                for identifier in dest_record['identifiers']:
+                    if identifier['scheme'] == 'RDA-MSCWG':
+                        id_no = int(identifier['id'].replace('msc:' + series, ''))
+                        if id_no > serial_no:
+                            serial_no = id_no
+                        break
+        exec(series + ' = serial_no')
+        print('Found {} up to msc:{}{}.'.format(folder, series, serial_no))
+    else:
         print ('Subdirectory {} missing, creating...'.format(folder))
         os.makedirs(os.path.join(args.dest, folder))
 
-## Utility functions
-
-# TODO: define vocabulary mapping
-def translateKeyword(kw):
-    output = None
-    return output
-
-def createSlug(string):
-    output = string.strip().lower().replace(' ', '-')
-    output = re.sub(r'-+', '-', output)
-    output = re.sub(r'[^-A-Za-z0-9_]+', '', output)
-    return output
+# TODO: scan for existing data and reuse identifiers?
 
 ## Parsing data files
-
-# Lookup for slug -> new ID
-m_index = dict()
-g_index = dict()
-t_index = dict()
-
-# Collecting information for post-processing
-mappings = list()
-sponsors = list()
-contacts = list()
-
-# Incremental ID integers
-m = 0
-g = 0
-t = 0
-c = 0
-e = 0
 
 # Parsing standards
 db_standards = dict()
 
 print('Converting standards to MSC data model...')
 for record in standards:
-    m += 1
     slug = os.path.splitext(os.path.basename(record))[0]
-    id_string = 'msc:m{}'.format(m)
+    id_string = getMSCID(os.path.join(args.dest, 'metadata-schemes', slug + '.yml'))
+    if not id_string:
+        m += 1
+        id_string = 'msc:m{}'.format(m)
     m_index[slug] = id_string
     dest_record = dict()
 
@@ -113,7 +161,8 @@ for record in standards:
         if 'version' in source_record:
             version = { 'number': source_record['version'] }
             if 'status' in source_record:
-                print('WARNING: check {} for how to handle status {}'.format(slug, source_record['status']))
+                log += 'Standard {} includes version status {}.\n'.format(slug, source_record['status'])
+                isNewLog = True
             if 'standard_update_date' in source_record:
                 version['issued'] = source_record['standard_update_date']
             dest_record['versions'] = [ version ]
@@ -162,12 +211,18 @@ for record in standards:
 
     db_standards[slug] = dest_record
 
+if isNewLog:
+    log += '\n'
+    isNewLog = False
+
 # Parsing profiles
 print('Converting profiles to MSC data model...')
 for record in profiles:
-    m += 1
     slug = os.path.splitext(os.path.basename(record))[0]
-    id_string = 'msc:m{}'.format(m)
+    id_string = getMSCID(os.path.join(args.dest, 'metadata-schemes', slug + '.yml'))
+    if not id_string:
+        m += 1
+        id_string = 'msc:m{}'.format(m)
     m_index[slug] = id_string
     dest_record = dict()
 
@@ -202,18 +257,25 @@ for record in profiles:
                     parent = { 'id': m_index[standard], 'role': 'parent scheme' }
                     dest_record['relatedEntities'].append(parent)
                 else:
-                    print('WARNING: unknown slug {} in profile {}.'.format(standard, slug))
+                    log += 'Extension {} contains reference to unknown parent standard {}.\n'.format(slug, standard)
+                    isNewLog = True
 
     db_standards[slug] = dest_record
+
+if isNewLog:
+    log += '\n'
+    isNewLog = False
 
 # Parsing tool records
 db_tools = dict()
 
 print('Converting tools to MSC data model...')
 for record in tools:
-    t += 1
     slug = os.path.splitext(os.path.basename(record))[0]
-    id_string = 'msc:t{}'.format(t)
+    id_string = getMSCID(os.path.join(args.dest, 'tools', slug + '.yml'))
+    if not id_string:
+        t += 1
+        id_string = 'msc:t{}'.format(t)
     t_index[slug] = id_string
     dest_record = dict()
 
@@ -239,18 +301,25 @@ for record in tools:
                     supported = { 'id': m_index[standard], 'role': 'supported scheme' }
                     dest_record['relatedEntities'].append(supported)
                 else:
-                    print('WARNING: unknown slug {} in tool {}.'.format(standard, slug))
+                    log += 'Tool {} contains reference to unknown standard {}.\n'.format(slug, standard)
+                    isNewLog = True
 
     db_tools[slug] = dest_record
+
+if isNewLog:
+    log += '\n'
+    isNewLog = False
 
 # Parsing use case records
 db_organizations = dict()
 
 print('Converting use cases to MSC data model...')
 for record in users:
-    g += 1
     slug = os.path.splitext(os.path.basename(record))[0]
-    id_string = 'msc:g{}'.format(g)
+    id_string = getMSCID(os.path.join(args.dest, 'organizations', slug + '.yml'))
+    if not id_string:
+        g += 1
+        id_string = 'msc:g{}'.format(g)
     g_index[slug] = id_string
     dest_record = dict()
 
@@ -278,19 +347,40 @@ for record in users:
                         db_standards[standard]['relatedEntities'] = list()
                     db_standards[standard]['relatedEntities'].append(relation)
                 else:
-                    print('WARNING: unknown slug {} in user {}.'.format(standard, slug))
+                    log += 'Use case {} contains reference to unknown standard {}.\n'.format(slug, standard)
+                    isNewLog = True
 
     db_organizations[slug] = dest_record
+
+if isNewLog:
+    log += '\n'
+    isNewLog = False
 
 # Creating mappings records
 db_mappings = dict()
 
+try:
+    mappings = sorted(mappings, key=lambda k: k['from'] + k['to'])
+except KeyError:
+    try:
+        mappings = sorted(mappings, key=lambda k: k['from'] + k['url'])
+    except KeyError:
+        mappings = sorted(mappings, key=lambda k: k['from'])
+
 print('Creating records for mappings...')
 for mapping in mappings:
-    c += 1
+    slug = ''
+    slug_from = mapping['from']
+    slug += '-'.join(slug_from.split('-')[:3])
+    slug += '_TO_'
+    slug_to = createSlug(mapping['to'])
+    slug += '-'.join(slug_to.split('-')[:3])
+    id_string = getMSCID(os.path.join(args.dest, 'mappings', slug + '.yml'))
+    if not id_string:
+        c += 1
+        id_string = 'msc:c{}'.format(c)
     dest_record = dict()
-    id_string = 'msc:c{}'.format(c)
-    
+
     record_id = { 'id': id_string, 'scheme': 'RDA-MSCWG' }
     dest_record['identifiers'] = [ record_id ]
     if 'url' in mapping:
@@ -300,43 +390,63 @@ for mapping in mappings:
     description = 'A mapping from '
     if 'from' in mapping:
         relation = dict()
-        slug = mapping['from']
-        if slug in m_index:
-            relation['id'] = m_index[slug]
-            description += db_standards[slug]['title']
+        if slug_from in m_index:
+            relation['id'] = m_index[slug_from]
+            description += db_standards[slug_from]['title']
         else:
-            print('WARNING: unknown slug {} in mapping'.format(slug))
-            description += slug
+            log += 'Mapping encountered from unknown standard {}. There is a bug in the migration script.\n'.format(slug_from)
+            isNewLog = True
+            description += slug_from
         relation['type'] = 'input scheme'
         related.append(relation)
     if 'to' in mapping:
         relation = dict()
         name = mapping['to']
-        slug = createSlug(name)
         description += ' to {}.'.format(name)
-        if slug in m_index:
-            relation['id'] = m_index[slug]
+        if slug_to in m_index:
+            relation['id'] = m_index[slug_to]
         else:
-            # unknown scheme: create stub
-            print('INFO: creating stub for {}. Is this right?'.format(slug))
-            m += 1
-            standard_id_string = 'msc:m{}'.format(m)
-            m_index[slug] = standard_id_string
-            standard_record = dict()
-            standard_record_id = { 'id': standard_id_string, 'scheme': 'RDA-MSCWG' }
-            standard_record['identifiers'] = [ standard_record_id ]
-            standard_record['title'] = name
-            db_standards[slug] = standard_record
-            relation['id'] = standard_id_string
+            to_id_string = getMSCID(os.path.join(args.dest, 'metadata-schemes', slug_to + '.yml'))
+            if to_id_string:
+                log += 'In standard {}, found mapping to unknown standard {} but stub already present.\n'.format(slug_from, slug_to)
+                isNewLog = True
+                relation['id'] = to_id_string
+                m_index[slug_to] = to_id_string
+                db_standards[slug_to] = loadRecord(os.path.join(args.dest, 'metadata-schemes', slug_to + '.yml'))
+            else:
+                # unknown scheme: create stub
+                log += 'In standard {}, found mapping to unknown standard {} so created a stub.\n'.format(slug_from, slug_to)
+                isNewLog = True
+                m += 1
+                standard_id_string = 'msc:m{}'.format(m)
+                m_index[slug_to] = standard_id_string
+                standard_record = dict()
+                standard_record_id = { 'id': standard_id_string, 'scheme': 'RDA-MSCWG' }
+                standard_record['identifiers'] = [ standard_record_id ]
+                standard_record['title'] = name
+                db_standards[slug_to] = standard_record
+                relation['id'] = standard_id_string
         relation['type'] = 'output scheme'
         related.append(relation)
     dest_record['relatedEntities'] = related
     dest_record['description'] = description
 
-    db_mappings[id_string] = dest_record
+    db_mappings[slug] = dest_record
+
+if isNewLog:
+    log += '\n'
+    isNewLog = False
 
 # Creating funder records
-print('Adding funding relationships...')
+try:
+    sponsors = sorted(sponsors, key=lambda k: k['standard'] + k['name'])
+except KeyError:
+    try:
+        sponsors = sorted(sponsors, key=lambda k: k['standard'] + k['url'])
+    except KeyError:
+        sponsors = sorted(sponsors, key=lambda k: k['standard'])
+
+print('Adding maintainer (sponsor) relationships...')
 for sponsor in sponsors:
     standard = sponsor['standard']
     id_string = None
@@ -345,6 +455,13 @@ for sponsor in sponsors:
         slug = createSlug(sponsor['name'])
         if slug in g_index:
             id_string = g_index[slug]
+        else:
+            id_string = getMSCID(os.path.join(args.dest, 'organizations', slug + '.yml'))
+            if id_string:
+                g_index[slug] = id_string
+                db_organizations[slug] = loadRecord(os.path.join(args.dest, 'organizations', slug + '.yml'))
+                log += 'In standard {}, found reference to unknown sponsor {} but stub already present.\n'.format(standard, slug)
+                isNewLog = True
     else:
         if 'url' in sponsor:
             for org_slug, org in db_organizations:
@@ -362,12 +479,15 @@ for sponsor in sponsors:
                                         break
     if (not id_string) and slug:
         # Need to create new record
-        print('INFO: creating stub for {}. Is this right?'.format(slug))
+        log += 'In standard {}, found reference to unknown sponsor {} so created a stub.\n'.format(standard, slug)
+        isNewLog = True
         g += 1
         id_string = 'msc:g{}'.format(g)
         g_index[slug] = id_string
         dest_record = dict()
         dest_record['name'] = sponsor['name']
+        record_id = { 'id': id_string, 'scheme': 'RDA-MSCWG' }
+        dest_record['identifiers'] = [ record_id ]
         locations = list()
         if 'url' in sponsor:
             location = { 'url': sponsor['url'], 'type': 'website' }
@@ -383,8 +503,20 @@ for sponsor in sponsors:
     else:
         print('WARNING: incomplete sponsor information in {}.'.format(standard))
 
+if isNewLog:
+    log += '\n'
+    isNewLog = False
+
 # Creating contact records
-print('Adding maintainer relationships...')
+try:
+    contacts = sorted(contacts, key=lambda k: k['standard'] + k['name'])
+except KeyError:
+    try:
+        contacts = sorted(contacts, key=lambda k: k['standard'] + k['email'])
+    except KeyError:
+        contacts = sorted(contacts, key=lambda k: k['standard'])
+
+print('Adding maintainer (contact) relationships...')
 for contact in contacts:
     standard = contact['standard']
     id_string = None
@@ -393,6 +525,13 @@ for contact in contacts:
         slug = createSlug(contact['name'])
         if slug in g_index:
             id_string = g_index[slug]
+        else:
+            id_string = getMSCID(os.path.join(args.dest, 'organizations', slug + '.yml'))
+            if id_string:
+                g_index[slug] = id_string
+                db_organizations[slug] = loadRecord(os.path.join(args.dest, 'organizations', slug + '.yml'))
+                log += 'In standard {}, found reference to unknown contact {} but stub already present.\n'.format(standard, slug)
+                isNewLog = True
     else:
         if 'email' in contact:
             for org_slug, org in db_organizations:
@@ -410,12 +549,15 @@ for contact in contacts:
                                         break
     if (not id_string) and slug:
         # Need to create new record
-        print('INFO: creating stub for {}. Is this right?'.format(slug))
+        log += 'In standard {}, found reference to unknown contact {} so created a stub.\n'.format(standard, slug)
+        isNewLog = True
         g += 1
         id_string = 'msc:g{}'.format(g)
         g_index[slug] = id_string
         dest_record = dict()
         dest_record['name'] = contact['name']
+        record_id = { 'id': id_string, 'scheme': 'RDA-MSCWG' }
+        dest_record['identifiers'] = [ record_id ]
         locations = list()
         if 'email' in contact:
             location = { 'url': contact['email'], 'type': 'email' }
@@ -443,7 +585,12 @@ for contact in contacts:
                 location = { 'url': contact['email'], 'type': 'email' }
                 db_organizations[slug]['locations'].append(location)
     else:
-        print('WARNING: incomplete contact information in {}.'.format(standard))
+        log += 'Standard {} has incomplete contact information.'.format(standard)
+        isNewLog = True
+
+if isNewLog:
+    log += '\n'
+    isNewLog = False
 
 ## Writing migrated data to files
 
@@ -460,10 +607,15 @@ for slug, dest_record in db_organizations.items():
     new_record = os.path.join(args.dest, 'organizations', slug + '.yml')
     with open(new_record, 'w') as r:
         yaml.safe_dump(dest_record, r)
-for id_string, dest_record in db_mappings.items():
-    fn = id_string.replace(':', '-') + '.yml'
-    new_record = os.path.join(args.dest, 'mappings', fn)
+for slug, dest_record in db_mappings.items():
+    new_record = os.path.join(args.dest, 'mappings', slug + '.yml')
     with open(new_record, 'w') as r:
         yaml.safe_dump(dest_record, r)
+
+print('There were issues you should look at: see migration log.')
+log = 'Migration log: {}\n\n'.format(datetime.datetime.now(datetime.timezone.utc).isoformat(' ')) + log
+if log:
+    with open(log_file, 'w') as l:
+        l.write(log)
 
 print('Finished!')
