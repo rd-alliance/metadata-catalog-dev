1558148ef2741978824cab0483543e6a2416fa0a
Alex Ball <alex-ball@users.noreply.github.com>
2017-02-03 14:18:46 +0000

    Add page templates

--- serve.py
+++ serve.py
@@ -12,7 +12,7 @@ import os, sys
 # On Debian, Ubuntu, etc.:
 #   - old version: sudo apt-get install python3-flask
 #   - latest version: sudo pip3 install flask
-from flask import Flask, request, url_for
+from flask import Flask, request, url_for, render_template, g
 
 # See http://tinydb.readthedocs.io/en/latest/intro.html
 # Install from PyPi: sudo pip3 install tinydb
@@ -29,12 +29,7 @@ db = TinyDB(os.path.realpath(os.path.join(script_dir, 'db.json')))
 
 @app.route('/')
 def hello():
-    page = '<html><head>'
-    page += '<title>Testing</title>'
-    style_url = url_for('static', filename='style.css')
-    page += '<link rel="stylesheet" type="text/css" href="{}" />'.format(style_url)
-    page += '</head><body><h1>Metadata Standards Catalog</h1></body></html>'
-    return page
+    return render_template('home.html')
 
 ### Display metadata scheme
 
@@ -42,8 +37,48 @@ def hello():
 def scheme(number):
     schemes = db.table('metadata-schemes')
     element = schemes.get(eid=number)
-    return '<html><head><title>Testing</title></head><body><h1>{}</h1></body></html>'.format(\
-        element['title'])
+    # Here we interpret the meaning of the versions
+    versions = None
+    if 'versions' in element:
+        versions = list()
+        raw_versions = element['versions']
+        for v in raw_versions:
+            this_version = dict()
+            if not 'number' in v:
+                continue
+            this_version['number'] = v['number']
+            this_version['status'] = ''
+            if 'issued' in v:
+                this_version['date'] = v['issued']
+                if 'valid' in v:
+                    if '/' in v['valid']:
+                        date_range = v['valid'].partition('/')
+                        this_version['status'] = 'deprecated on '.format(date_range[2])
+                    else:
+                        this_version['status'] = 'current'
+            elif 'valid' in v:
+                if '/' in v['valid']:
+                    date_range = v['valid'].partition('/')
+                    this_version['date'] = date_range[0]
+                    this_version['status'] = 'deprecated on '.format(date_range[2])
+                else:
+                    this_version['date'] = v['valid']
+                    this_version['status'] = 'current'
+            elif 'available' in v:
+                this_version['date'] = v['available']
+                this_version['status'] = 'proposed'
+            versions.append(this_version)
+        versions = sorted(versions, key=lambda k: k['date'], reverse=True)
+        for version in versions:
+            if version['status'] == 'current':
+                break
+            if version['status'] == 'proposed':
+                continue
+            if version['status'] == '':
+                version['status'] = 'current'
+                break
+    return render_template('metadata-scheme.html', record=element,\
+        versions=versions)
 
 ### Search form
 
@@ -57,4 +92,4 @@ def search():
 ### Executing
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
